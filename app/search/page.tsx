91e65d3bb3bb09f7061afee15ffa0f1100215667
Chung Hsuan <qazwsx5219430630@gmail.com>
2023-03-10 15:18:51 +0800

    searchBar fetch prisma prac

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,19 +1,31 @@
 import React from "react";
-
 import NavBar from "../components/NavBar";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
+import { PrismaClient } from "@prisma/client";
 
-function Search() {
+const prisma = new PrismaClient();
+
+const fetchSearchRestaurant = async (city: string) => {
+    if (!city) return prisma.restaurant.findMany();
+    return await prisma.restaurant.findMany({
+        where: { location: { name: { equals: city } } },
+    });
+};
+
+async function Search({ searchParams }: { searchParams: { city: string } }) {
+    const restaurants = await fetchSearchRestaurant(searchParams.city.toLowerCase());
+
     return (
         <>
             <Header />
             <div className="flex py-4 m-auto w-2/3 justify-between items-start">
                 <SearchSideBar />
-
                 <div className="w-5/6">
-                    <RestaurantCard />
+                    {restaurants.map((restaurant) => (
+                        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+                    ))}
                 </div>
             </div>
         </>
